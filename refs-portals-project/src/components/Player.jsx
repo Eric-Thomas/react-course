252298177c578f9501a156cd7bb4ae7282b4be01
Eric Thomas <ethomasosu@gmail.com>
2026-06-01 16:07:42 -0400

    use shorthand ?? for ternary expression to say return the right hand side if the left is null or undefined, otherwise return left side

--- refs-portals-project/src/components/Player.jsx
+++ refs-portals-project/src/components/Player.jsx
@@ -1,14 +1,14 @@
 import { useState, useRef } from "react";
 export default function Player() {
   const playerName = useRef();
-  const [entityName, setEntityName] = useState("");
+  const [entityName, setEntityName] = useState(null);
 
   function handleClick() {
     setEntityName(playerName.current.value);
   }
   return (
     <section id="player">
-      <h2>Welcome { entityName ? entityName : "Unknown Entity"} </h2>
+      <h2>Welcome { entityName ??  "Unknown Entity"} </h2>
       <p>
         <input ref={playerName} type="text"/>
         <button onClick={handleClick}>Set Name</button>
