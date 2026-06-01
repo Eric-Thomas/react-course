b8456908023023e6b74fee7715121621b9ef698f
Eric Thomas <ethomasosu@gmail.com>
2026-06-01 15:55:09 -0400

    handle name state using already known methods
    Preparing to use refs in the next section

--- refs-portals-project/src/components/Player.jsx
+++ refs-portals-project/src/components/Player.jsx
@@ -1,10 +1,22 @@
+import { useState } from "react";
 export default function Player() {
+  const [entityName, setEntityName] = useState("");
+  const [submitted, setSubmitted] = useState(false);
+
+  function handleChange(event) {
+    setSubmitted(false);
+    setEntityName(event.target.value);
+  }
+
+  function handleClick() {
+    setSubmitted(true);
+  }
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {submitted ? entityName: "Unknown Entity"} </h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input type="text" onChange={handleChange} value={entityName}/>
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
