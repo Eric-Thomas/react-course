8c1bfead1556453c15821b4ace5d6ade700f2b58
Eric Thomas <ethomasosu@gmail.com>
2026-06-05 15:19:34 -0400

    Add timer challenges
    Need to solve for a way to inerrupt the setTimeout() when the button is clicked again. We will use help of refs

--- refs-portals-project/src/components/Player.jsx
+++ refs-portals-project/src/components/Player.jsx
@@ -8,9 +8,9 @@ export default function Player() {
   }
   return (
     <section id="player">
-      <h2>Welcome { entityName ??  "Unknown Entity"} </h2>
+      <h2>Welcome {entityName ?? "Unknown Entity"} </h2>
       <p>
-        <input ref={playerName} type="text"/>
+        <input ref={playerName} type="text" />
         <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
