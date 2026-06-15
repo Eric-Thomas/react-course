f1786a74bd0c1d4e6f50e1643446ff2ac2dd933a
Eric Thomas <ethomasosu@gmail.com>
2026-06-15 10:02:43 -0400

    create projects state

--- project-management-project/src/App.jsx
+++ project-management-project/src/App.jsx
@@ -1,10 +1,12 @@
+import { useState } from "react";
 import Sidebar from "./components/Sidebar";
 import WelcomeMessage from "./components/WelcomeMessage";
 function App() {
+  const [projects, setProjects] = useState([]);
   return (
     <>
       <main className="h-screen my-8 flex gap-8">
-        <Sidebar></Sidebar>
+        <Sidebar projects={projects}></Sidebar>
         <WelcomeMessage />
       </main>
     </>
--- project-management-project/src/components/Sidebar.jsx
+++ project-management-project/src/components/Sidebar.jsx
@@ -1,4 +1,4 @@
-export default function Sidebar() {
+export default function Sidebar({ projects }) {
   return (
     <aside className="w-1/3 px-8 py-16 bg-stone-900 text-stone-50 md:w-72 rounded-r-xl">
       <h2 className="mb-8 font-bold uppercase md:text-xl text-stone-200">
@@ -7,6 +7,15 @@ export default function Sidebar() {
       <button className="px-4 py-2 text-xs md:text-base rounded-md bg-stone-700 text-stone-400 hover:bg-stone-600 hover:text-stone-100">
         + Add Project
       </button>
+      <ul className="mt-8">
+        {projects.map((project) => {
+          return (
+            <li className="w-full text-left px-2 py-1 rounded-sm my-1 hover:text-stone-200 hover:bg-stone-800 text-stone-400">
+              {project.title}
+            </li>
+          );
+        })}
+      </ul>
     </aside>
   );
 }
